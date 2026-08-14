5c8c93d445e05fc3ac9c9c84a9368465241b7810
W3ndyM1jangos <wendy.mijangos.imbpc@gmail.com>
2026-08-13 23:43:46 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
+<?php
+$mensaje = "⚠️ ESTA PÁGINA ESTÁ HECHA ÚNICAMENTE CON FINES EDUCATIVOS ⚠️";
+$url_inicio = "index.html";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Aviso Educativo</title>
+  <link rel="stylesheet" href="css/aviso.css">
+</head>
+<body>
 
+  <div class="box">
+    <marquee class="rojo"><?php echo $mensaje; ?></marquee>
+    <a href="<?php echo $url_inicio; ?>" class="btn">Ingresar al sitio</a>
+  </div>
+
+</body>
+</html>
